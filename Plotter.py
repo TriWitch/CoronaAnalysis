3bc5da2128c31801c69fdaddeb844cf3118d0361
TriWitch <webmaster@upontheheath.de>
2020-07-11 17:21:31 +0200

    Cleanup and renaming

--- Plotter.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import pandas as pd
-with urllib.request.urlopen('https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv') as f:
-    #html = f.read().decode('utf-8')
-    df = pd.read_csv(f, encoding = 'utf-8')
-    df.to_csv("Current.csv", index = False)
-    
